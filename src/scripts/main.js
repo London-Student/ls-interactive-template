bb6e884313856bef325fc6bbbf51ab6ec57cf654
Cale Tilford <cale.tilford@ft.com>
2017-11-01 17:43:49 +0000

    WIP adding new structure

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,9 @@ var bodyTemplate = require("../templates/body.handlebars");
 var headTemplate = require("../templates/head.handlebars");
 
 document.addEventListener("DOMContentLoaded", function() {
-  document.body.innerHTML = bodyTemplate({message: "Welcome to London Student!"});
+  document.body.innerHTML = bodyTemplate({
+    title: "London Student has given thousands of young journalists the chance to write and report on their campuses",
+    message: "Welcome to London Student!"
+  });
   document.head.innerHTML = document.head.innerHTML + headTemplate();
 });

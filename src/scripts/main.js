e79a05ce30e55edf8f5963f94db49105a186083a
Cale Tilford <combinekidd@gmail.com>
2017-11-06 22:31:33 +0000

    Add basic meta info to template

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,8 @@ var headTemplate = require("../templates/head.handlebars");
 document.addEventListener("DOMContentLoaded", function() {
   document.body.innerHTML = bodyTemplate({
     title: "London Student has given thousands of young journalists the chance to write and report on their campuses",
+    author: "Cale Tilford",
+    datePublished: "November 6, 2017",
     message: "Welcome to London Student!"
   });
   document.head.innerHTML = document.head.innerHTML + headTemplate();

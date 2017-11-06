a3e76b4934789cad3f549454747e4f55dbbcab7d
Cale Tilford <combinekidd@gmail.com>
2017-11-06 23:49:14 +0000

    Add content paragraphs and fonts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,8 +7,12 @@ document.addEventListener("DOMContentLoaded", function() {
   document.body.innerHTML = bodyTemplate({
     title: "London Student has given thousands of young journalists the chance to write and report on their campuses",
     author: "Cale Tilford",
-    datePublished: "November 6, 2017",
-    message: "Welcome to London Student!"
+    datePublished: "Nov 6, 2017",
+    paragraphs: [
+      "Since 1979, London Student has given thousands of young journalists the chance to write and report on their campuses.",
+      "Now we are an independent co-operative, run by our members. We believe students’ unions and universities are so bad at running student publications that we’re better off doing it ourselves.",
+      "We want to produce journalism that’s undeniably relevant to our readers. London students care about what their tuition fees are being spent on, free speech, safety, the environment, activism and student politics."
+    ]
   });
   document.head.innerHTML = document.head.innerHTML + headTemplate();
 });

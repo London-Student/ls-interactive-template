f50c0861392e49e661722ae2ee380055def98423
Cale Tilford <combinekidd@gmail.com>
2017-11-08 20:14:41 +0000

    Add article footnote

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,6 +12,10 @@ document.addEventListener("DOMContentLoaded", function() {
       "Since 1979, London Student has given thousands of young journalists the chance to write and report on their campuses.",
       "Now we are an independent co-operative, run by our members. We believe students’ unions and universities are so bad at running student publications that we’re better off doing it ourselves.",
       "We want to produce journalism that’s undeniably relevant to our readers. London students care about what their tuition fees are being spent on, free speech, safety, the environment, activism and student politics."
+    ],
+    footnote: [
+      "Sources: A great source for a great article",
+      "Credits: Someone great who did a great thing"
     ]
   });
   document.head.innerHTML = document.head.innerHTML + headTemplate();

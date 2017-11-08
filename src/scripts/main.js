447b7eef93757ba95112682f9479ab9eef8dfbd6
Cale Tilford <combinekidd@gmail.com>
2017-11-08 21:42:34 +0000

    Define object notation for svg

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,9 +9,28 @@ document.addEventListener("DOMContentLoaded", function() {
     author: "Cale Tilford",
     datePublished: "Nov 6, 2017",
     paragraphs: [
-      "Since 1979, London Student has given thousands of young journalists the chance to write and report on their campuses.",
-      "Now we are an independent co-operative, run by our members. We believe students’ unions and universities are so bad at running student publications that we’re better off doing it ourselves.",
-      "We want to produce journalism that’s undeniably relevant to our readers. London students care about what their tuition fees are being spent on, free speech, safety, the environment, activism and student politics."
+      {
+        text: "Since 1979, London Student has given thousands of young journalists the chance to write and report on their campuses."
+      },
+      {
+        svg: true,
+        width: 800,
+        height: 400
+      },
+      {
+        text: "Now we are an independent co-operative, run by our members. We believe students’ unions and universities are so bad at running student publications that we’re better off doing it ourselves."
+      },
+      {
+        text: "We want to produce journalism that’s undeniably relevant to our readers. London students care about what their tuition fees are being spent on, free speech, safety, the environment, activism and student politics."
+      },
+      {
+        svg: true,
+        width: 800,
+        height: 400
+      },
+      {
+        text: "We want to produce journalism that’s undeniably relevant to our readers. London students care about what their tuition fees are being spent on, free speech, safety, the environment, activism and student politics."
+      }
     ],
     footnote: [
       "Sources: A great source for a great article",

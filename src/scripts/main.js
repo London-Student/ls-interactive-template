d99d262ff32818def04718b34054f191bb74bd48
Cale Tilford <combinekidd@gmail.com>
2017-11-13 23:39:25 +0000

    Move article data to json file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,11 @@
 import style from '../styles/main.scss';
 
-var bodyTemplate = require("../templates/body.handlebars");
-var headTemplate = require("../templates/head.handlebars");
+const bodyTemplate = require("../templates/body.handlebars");
+const headTemplate = require("../templates/head.handlebars");
+
+let articleData = require('../../static/article.json');
 
 document.addEventListener("DOMContentLoaded", function() {
-  document.body.innerHTML = bodyTemplate({
-    title: "London Student has given thousands of young journalists a chance",
-    author: "Cale Tilford",
-    datePublished: "Nov 6, 2017",
-    paragraphs: [
-      {
-        text: "Since 1979, London Student has given thousands of young journalists the chance to write and report on their campuses."
-      },
-      {
-        svg: true
-      },
-      {
-        text: "Now we are an independent co-operative, run by our members. We believe students’ unions and universities are so bad at running student publications that we’re better off doing it ourselves."
-      },
-      {
-        text: "We want to produce journalism that’s undeniably relevant to our readers. London students care about what their tuition fees are being spent on, free speech, safety, the environment, activism and student politics."
-      },
-      {
-        svg: true
-      },
-      {
-        text: "We want to produce journalism that’s undeniably relevant to our readers. London students care about what their tuition fees are being spent on, free speech, safety, the environment, activism and student politics."
-      }
-    ],
-    footnote: [
-      "Sources: A great source for a great article",
-      "Credits: Someone great who did a great thing"
-    ]
-  });
-  document.head.innerHTML = document.head.innerHTML + headTemplate();
+  document.body.innerHTML = bodyTemplate(articleData);
+  document.head.innerHTML = document.head.innerHTML + headTemplate(articleData);
 });
